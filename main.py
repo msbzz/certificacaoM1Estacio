5987a46869dcba28dfd0047187f2aaf5612261cf
Fábio Morales Prado <44789547+fabiohprado@users.noreply.github.com>
2022-11-08 20:45:08 -0300

    Att menu main
    Fiz um layout inicial do menu main já puxando algumas funções ativas

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
 from tkinter import *
-
+import tkinter as tk
+import tkinter as tkk
 from ferramentas import cadastroFerramentas,consultarFerramentas,listarFerramentas 
 from funcionarios import cadastroFuncionarios
 from solicitacoes import cadastroSolicitacoes
-
-master =  Tk()
-master.title("---Controle de Ferramentas---")
+#janela
+master = tk.Tk()
+master.iconbitmap("cerificacaoM1Estacio/imagens/es.ico")
+master.title("Controle de Ferramentas e Funcionários")
 master.geometry('995x643+491+115')
+#fonte
+fontP =('calibri', 12, 'normal')
+#Responsividade
+jFerramentas = tk.Frame(master)
+jFuncionarios = tk.Frame(master)
+jSolicitacoes = tk.Frame(master)
 
-barraMenu =  Menu(master)
-
-
-menuFerramentas= Menu(barraMenu)
-
-menuFuncionarios= Menu(barraMenu)
-
-menuSolicitacoes= Menu(barraMenu)
-
-menuFerramentas.add_command(label='Cadastrar Ferramentas',command=cadastroFerramentas)
-menuFerramentas.add_separator()
-menuFerramentas.add_command(label='Consultar Ferramentas',command=consultarFerramentas)
-menuFerramentas.add_separator()
-menuFerramentas.add_command(label='listar Ferramentas',command=listarFerramentas)
-
-menuFuncionarios.add_command(label='Cadastrar Funcionários',command=cadastroFuncionarios)
-menuFuncionarios.add_separator()
-menuFuncionarios.add_command(label='Consultar Funcionário',command='')
-menuFuncionarios.add_separator()
-menuFuncionarios.add_command(label='listar Funcionários',command='')
-
-menuSolicitacoes.add_command(label='Cadastrar Solicitações',command='')
-menuSolicitacoes.add_separator()
-menuSolicitacoes.add_command(label='Consultar Solicitação',command='')
-menuSolicitacoes.add_separator()
-menuSolicitacoes.add_command(label='listar Solicitações',command='')
+#Seção de ferramentas
+scFerramentas = tk.Listbox(jFerramentas)
+#photo = tk.PhotoImage(master, arquivo="/cerificacaoM1Estacio/imagens/ferramentas.jpg")
+bFCadastro = tk.Button(jFerramentas, text="Cadastro de Ferramentas", font=fontP, command=cadastroFerramentas).grid(row=1, column=0)
+bFConsulta = tk.Button(jFerramentas, text="Consulta de Ferramentas", font=fontP).grid(row=2, column=0)
+bFListar = tk.Button(jFerramentas, text="Listar de Ferramentas", font=fontP).grid(row=3, column=0)
 
 
-barraMenu.add_cascade(label='Ferramentas',menu=menuFerramentas)
-barraMenu.add_cascade(label='Funcionarios',menu=menuFuncionarios)
-barraMenu.add_cascade(label='Solicitações',menu=menuSolicitacoes)
-barraMenu.add
-master.config(menu=barraMenu)
+#Funcionarios
+scFuncionarios = tk.Listbox(jFuncionarios)
+bCadastroF = tk.Button(jFuncionarios, text="Cadastrar Funcionário", font=fontP, command=cadastroFuncionarios).grid(row=1, column=1)
+bConsultaF = tk.Button(jFuncionarios, text="Consultar Funcionário", font=fontP).grid(row=2, column=1)
+bListF = tk.Button(jFuncionarios, text="Listar Funcionário", font=fontP).grid(row=3, column=1)
+
+#solicitações
+scSolicitacoes = tk.Listbox(jSolicitacoes)
+bSCadastro = tk.Button(jSolicitacoes, text="Cadastrar Solicitações", font=fontP).grid(row=5, column=1)
+bSconsult = tk.Button(jSolicitacoes, text="Consultar Solicitações", font=fontP).grid(row=6, column=1)
+bSList = tk.Button(jSolicitacoes, text="Listar Solicitações", font=fontP).grid(row=7, column=1)
+
+
+jFerramentas.pack(anchor=NW, padx=50, pady=30)
+jFuncionarios.pack(anchor=NE, padx=50, pady=30)
+jSolicitacoes.pack(anchor=SW, padx=50, pady=30)
+
+
+
+
 master.mainloop()
