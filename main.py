d794c5167281d48cef35a3c08de84aef1d532f71
Marco Barozzi <mbarozzi@gmail.com>
2022-11-12 01:40:00 -0300

    formulario main
    Cada modulo de consulta (ferramenta,funcionários e solicitações,) vai ter chamada para listagem.Removendo listagem.  do form main()

--- main.py
+++ main.py
@@ -33,20 +33,20 @@ scFerramentas = tk.Listbox(jFerramentas)
 #photo = tk.PhotoImage(master, arquivo="/cerificacaoM1Estacio/imagens/ferramentas.jpg")
 bFCadastro = tk.Button(jFerramentas, text="Cadastro de Ferramentas", font=fontP, command=cadastroFerramentas).grid(row=1, column=0)
 bFConsulta = tk.Button(jFerramentas, text="Consulta de Ferramentas", font=fontP,command=consultarFerramentas).grid(row=2, column=0)
-bFListar = tk.Button(jFerramentas, text="Listar de Ferramentas", font=fontP).grid(row=3, column=0)
+#bFListar = tk.Button(jFerramentas, text="Listar de Ferramentas", font=fontP).grid(row=3, column=0)
 
 
 #Funcionarios
 scFuncionarios = tk.Listbox(jFuncionarios)
 bCadastroF = tk.Button(jFuncionarios, text="Cadastrar Funcionário", font=fontP, command=cadastroFuncionarios).grid(row=1, column=1)
 bConsultaF = tk.Button(jFuncionarios, text="Consultar Funcionário", font=fontP,command=consultarFuncionarios).grid(row=2, column=1)
-bListF = tk.Button(jFuncionarios, text="Listar Funcionário", font=fontP).grid(row=3, column=1)
+#bListF = tk.Button(jFuncionarios, text="Listar Funcionário", font=fontP).grid(row=3, column=1)
 
 #solicitações
 scSolicitacoes = tk.Listbox(jSolicitacoes)
 bSCadastro = tk.Button(jSolicitacoes, text="Cadastrar Solicitações", font=fontP, command=cadastroSolicitacoes).grid(row=5, column=1)
 bSconsult = tk.Button(jSolicitacoes, text="Consultar Solicitações", font=fontP,command=consultarSolicitacoes).grid(row=6, column=1)
-bSList = tk.Button(jSolicitacoes, text="Listar Solicitações", font=fontP).grid(row=7, column=1)
+#bSList = tk.Button(jSolicitacoes, text="Listar Solicitações", font=fontP).grid(row=7, column=1)
 
 
 jFerramentas.pack(anchor=NW, padx=50, pady=30)
