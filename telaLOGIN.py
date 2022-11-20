8d7b83622fe2c40d6825f4b39581c794b3895b9f
Fer G. Vargas <fernandadguimaraes@hotmail.com>
2022-11-20 14:00:07 -0300

    Tela Login atualizada
    Padronizado tela Login conforme layout do programa.
    Incluído link de tela de Login utilizada

--- telaLOGIN.py
+++ telaLOGIN.py
@@ -4,15 +4,17 @@ from tkinter import messagebox
 
 # Cores -----------------------------
 co0 = "#f0f3f5"  # Preta / black
-co1 = "#feffff"  # branca / white
-co2 = "#3fb5a3"  # verde / green
+co1 = "#497174"  # branca / white
+co2 = "#EB6440"  # verde / green 
 co3 = "#38576b"  # valor / value
 co4 = "#403d3d"   # letra / letters
 
 # Criando janelas ---------------------
 janela = Tk()
-janela.title('')
+janela.title('Tela de LOGIN')
 janela.geometry('310x300')
+photo = PhotoImage(file='imagens/toolsIco-48.png')
+janela.iconphoto(False, photo)
 janela.configure(background=co1)
 janela.resizable(width=FALSE,height=FALSE)
 
@@ -24,10 +26,10 @@ frame_baixo = Frame(janela,width=310,height=250,bg=co1,relief='flat')
 frame_baixo.grid(row=1,column=0,pady=1,padx=0,sticky=NSEW)
 
 # Configurando o frame cima ---------------------
-l_nome = Label(frame_cima, text='LOGIN', anchor=NE,font=('Ivy 25'),bg=co1,fg=co4)
+l_nome = Label(frame_cima, text='LOGIN', anchor=NE,font=('calibri 25 normal'),bg=co1)
 l_nome.place(x=5,y=5)
 
-l_linha = Label(frame_cima, text='', width=275,anchor=NW,font=('Ivy 1'),bg=co2,fg=co4)
+l_linha = Label(frame_cima, text='', width=275,anchor=NW,font=('calibri 12 normal'),bg=co2)
 l_linha.place(x=10,y=45)
 
 
@@ -58,30 +60,30 @@ def verificar_senha():
 # Função de verificação ---------------------
 def nova_janela():
     # Configurando o frame cima
-    l_nome = Label(frame_cima, text='Usuario : ' +credenciais[0], anchor=NE,font=('Ivy 20'),bg=co1,fg=co4)
+    l_nome = Label(frame_cima, text='Usuario : ' +credenciais[0], anchor=NE,font=('calibri 10'),bg=co1)
     l_nome.place(x=5,y=5)
 
-    l_linha = Label(frame_cima, text='', width=275,anchor=NW,font=('Ivy 1'),bg=co2,fg=co4)
+    l_linha = Label(frame_cima, text='', width=275,anchor=NW,font=('calibri 1'),bg=co2)
     l_linha.place(x=10,y=45)
 
-    l_nome = Label(frame_baixo, text='Seja bem vindo ' +credenciais[0], anchor=NE,font=('Ivy 20'),bg=co1,fg=co4)
+    l_nome = Label(frame_baixo, text='Seja bem vindo ' +credenciais[0], anchor=NE,font=('calibri 10'),bg=co1)
     l_nome.place(x=5,y=105)
 
 
 
 
 # Configurando o frame baixo ---------------------
-l_nome = Label(frame_baixo, text='Nome *', anchor=NW,font=('Ivy 10'),bg=co1,fg=co4)
+l_nome = Label(frame_baixo, text='Nome ', anchor=NW,font=('calibri 12 normal'),bg=co1)
 l_nome.place(x=10,y=20)
-e_nome = Entry(frame_baixo, width=25,justify='left',font=("", 15),highlightthickness=1, relief='solid')
+e_nome = Entry(frame_baixo, width=25,justify='left',font=("", 15))
 e_nome.place(x=14,y=50)
 
-l_pass = Label(frame_baixo, text='Palavra passe *', anchor=NW,font=('Ivy 10'),bg=co1,fg=co4)
-l_pass.place(x=10,y=95)
-e_pass = Entry(frame_baixo, width=25,justify='left',show='*',font=("", 15),highlightthickness=1, relief='solid')
+l_pass = Label(frame_baixo, text='Senha ', anchor=NW,font=('calibri 12 normal'),bg=co1)
+l_pass.place(x=10,y=100)
+e_pass = Entry(frame_baixo, width=25,justify='left',show='*',font=("", 15))
 e_pass.place(x=14,y=130)
 
-b_confirmar = Button(frame_baixo,command=verificar_senha ,text='Entrar', width=39,height=2,font=('Ivy 8 bold'),bg=co2,fg=co1, relief=RAISED, overrelief= RIDGE)
+b_confirmar = Button(frame_baixo,command=verificar_senha ,text='Entrar', width=39,height=2,font=('calibri 10 bold'),bg=co2)
 b_confirmar.place(x=15,y=180)
 
 
