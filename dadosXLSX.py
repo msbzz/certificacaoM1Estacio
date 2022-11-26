31f145c1b4daf7ef0915fb30a53822c7b7fd0442
Marco Barozzi <mbarozzi@gmail.com>
2022-11-26 16:30:28 -0300

    faltou atualizar

--- dadosXLSX.py
+++ dadosXLSX.py
@@ -56,8 +56,18 @@ class Dados():
         pathDestinoArq  = self.getPathPlanilhas(nome_e_Extensao,self.pathDestino)
 
         book.save(pathDestinoArq)
+    def DeleteOneXLSX(self, nome_e_Extensao, nomePlanilha,index):
+        #RETORNA UMA LISTA DE LISTAS DE REGISTROS
+        #MATRIZ
+        #CADA LINHA É UM REGISTRO OU LINHA DA PLANILHA
+        book = self.openpyXL(nome_e_Extensao,nomePlanilha)
+        infoCells = book[nomePlanilha]
+        infoCells.delete_rows(int(index), 1)
 
-    def OpenReadXLSX(self, nome_e_Extensao, nomePlanilha,lsCab=[]):
+        book.save('planilhas/'+nome_e_Extensao)
+
+
+    def OpenReadXLSX(self, nome_e_Extensao, nomePlanilha,lsCab=[],linha=0):
         #RETORNA UMA LISTA DE LISTAS DE REGISTROS
         #MATRIZ
         #CADA LINHA É UM REGISTRO OU LINHA DA PLANILHA
@@ -68,29 +78,37 @@ class Dados():
         #print(' cols   ====>>',len(lsCab))
         
         nCols=len(lsCab)
+        
+        if linha!=0:
+            nCols-=1
 
         lsReturn = []
-        try:
-           #REDUZIR O NUMERO DE LINHAS DO GRID / para testar retorno usa max_row=1
-           #min_row=1
-           for rows in infoCells.iter_rows():
+        #try:
+        #REDUZIR O NUMERO DE LINHAS DO GRID / para testar retorno usa max_row=1
+        #min_row=1
+        contLinha=1
+        for rows in infoCells.iter_rows():
             
-               ls=''
-             
-               for i in range(0,nCols):
-                   if (len(ls)>0):
-                      ls+= ','+rows[i].value
-                   else:
-                      ls= rows[i].value 
-
-               if (rows[0].value!=None):
-                  lsReturn.append(ls.upper())
-               else: 
-                  break
+            ls=''
+            
+            for i in range(0,nCols):
+                if (len(ls)>0):
+                    ls+= ','+rows[i].value
+                else:
+                    if linha!=1:  
+                       ls= rows[i].value 
+                    else:
+                       ls= str(contLinha)+ ',' + rows[i].value 
+                    contLinha+=1  
+                        
+            if (rows[0].value!=None):
+                lsReturn.append(ls.upper())
+            else: 
+                break
 
                #print('dados ========>>>>>',ls.split(','))
-        except:
-            pass 
+        #except:
+        #    pass 
         return lsReturn     
 
     def getList(self, nome_e_Extensao, nomePlanilha,col=0):
